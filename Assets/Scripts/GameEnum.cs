d2e629cc4eade744d7e618fe13012068eef68d37
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-16 14:49:28 +0800

    Added GameEnum script

---
+++ Assets/Scripts/GameEnum.cs
@@ -0,0 +1,22 @@
+namespace GameEnum
+{
+    public enum ActionEvent
+    {
+        IngredientAdded,
+    }
+    public enum ParameterKey
+    {
+        IngredientType,
+    }
+    public enum IngredientType
+    {
+        Batter,
+        Butter
+    }
+
+    public enum ActionState
+    {
+        Success,
+        Failure
+    }
+}
