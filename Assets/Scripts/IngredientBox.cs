e9a416bbe6843e3698f7dcf6cd54ec2b7ada4ba3
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-17 21:31:03 +0800

    Added IngredientBox script

---
+++ Assets/Scripts/IngredientBox.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using GameEnum;
+using UnityEngine.Pool;
+
+public class IngredientBox : MonoBehaviour, Clickable
+{
+    [SerializeField] private IngredientType ingredientType;
+    private ObjectPool pool;
+    private void Start()
+    {
+        Debug.Log($"ClickableObject {this.gameObject.name} is running");
+        this.pool = GlobalObjectPools.GetPoolByIngredientType(ingredientType);
+    }
+    public void OnClicked()
+    {
+        Debug.Log($"ClickableObject {this.gameObject.name} was clicked");
+
+        //Parameters parameters = new Parameters();
+        //string key = ParameterKey.IngredientType.ToString();
+        //parameters.PutExtra(key, this.ingredientType.ToString());
+        
+        //Debug.Log($"Posting event from IngredientBox with parameter {key}: {this.ingredientType}");
+        //EventBroadcaster.Instance.PostEvent(ActionEvent.IngredientAdded.ToString(), parameters);
+        GameObject ingredient = pool.GetObject();
+        InteractionManager.GrabObject(ingredient);
+    }
+
+    public void OnClickRelease()
+    {
+        // Should not be called
+        Debug.Log($"ClickableObject {this.gameObject.name} click released");
+    }
+}
