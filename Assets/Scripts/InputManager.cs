8b745f94c88f16fe7fbf7dd9b4cfaf4e382534ca
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-17 19:22:03 +0800

    Added InputManager script

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InputManager : MonoBehaviour
+{
+    public event System.Action<Vector2> OnInputDown;
+    public event System.Action<Vector2> OnInputUp;
+
+    public void OnClick(InputValue value)
+    {
+        Debug.Log($"InputManager received click input with value: {value.isPressed}");
+        Vector2 mousePos = Mouse.current.position.ReadValue();
+
+        if (value.isPressed)
+        {
+            OnInputDown?.Invoke(mousePos);
+        } else
+        {
+            OnInputUp?.Invoke(mousePos);
+        }
+    }
+}
