6d60947cea877dd4c48cc0e5f2d254c70b114f00
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-19 00:05:52 +0800

    Split Ingredient.cs to Grabber.cs

--- Assets/Scripts/Ingredient.cs
+++ Assets/Scripts/Ingredient.cs
@@ -1,6 +1,6 @@
 using GameEnum;
 using UnityEngine;
-public class Ingredient : MonoBehaviour, Clickable
+public class Ingredient : MonoBehaviour
 {
     [SerializeField] private IngredientType ingredientType;
     public IngredientType IngredientType { get {return ingredientType; } set { ingredientType = value; }}
@@ -8,17 +8,5 @@ public class Ingredient : MonoBehaviour, Clickable
     {
         ObjectPool pool = GlobalObjectPools.GetPoolByIngredientType(ingredientType);
         pool.ReturnObject(this.gameObject);
-    }
-
-    public void OnClicked()
-    {
-        Debug.Log($"Ingredient {this.gameObject.name} was clicked");
-        InteractionManager.GrabObject(this.gameObject);
-    }
-
-    public void OnClickRelease()
-    {
-        Debug.Log($"Ingredient {this.gameObject.name} click released");
-        InteractionManager.ReleaseObject(this.gameObject);
     }
 }
