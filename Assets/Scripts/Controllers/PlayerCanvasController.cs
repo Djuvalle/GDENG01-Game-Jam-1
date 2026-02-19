93bd7f814c248a15e4d67cde4969ed82aabafdc9
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-20 06:25:34 +0800

    Fixed logic error in Start

--- Assets/Scripts/Controllers/PlayerCanvasController.cs
+++ Assets/Scripts/Controllers/PlayerCanvasController.cs
@@ -7,11 +7,12 @@ public class PlayerCanvasController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        this.tmpObjective = GetComponent<TextMeshProUGUI>();
+        this.tmpObjective = this.transform.Find("ObjectiveText").gameObject.GetComponent<TextMeshProUGUI>();
         EventBroadcaster.Instance.AddObserver(ActionEvent.UpdateView.ToString(), this.HandleUpdateView);
     }
     private void HandleUpdateView(Parameters parameters)
     {
+        Debug.Log("Update View");
         int cur = parameters.GetIntExtra("Current", -99);
         int max = parameters.GetIntExtra("Goal", -99);
         bool isWin = parameters.GetBoolExtra("Win", false);
