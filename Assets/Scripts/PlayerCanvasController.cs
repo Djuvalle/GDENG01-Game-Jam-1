08f0b4557ec70852467485b929d771db548b7947
Djuvalle <djuvalle_buenavente@dlsu.edu.ph>
2026-02-20 05:44:01 +0800

    Created PlayerCanvasController.cs

---
+++ Assets/Scripts/PlayerCanvasController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using GameEnum;
+using TMPro;
+public class PlayerCanvasController : MonoBehaviour
+{
+    private TextMeshProUGUI tmpObjective;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        this.tmpObjective = GetComponent<TextMeshProUGUI>();
+        EventBroadcaster.Instance.AddObserver(ActionEvent.UpdateView.ToString(), this.HandleUpdateView);
+    }
+    private void HandleUpdateView(Parameters parameters)
+    {
+        int cur = parameters.GetIntExtra("Current", -99);
+        int max = parameters.GetIntExtra("Goal", -99);
+        bool isWin = parameters.GetBoolExtra("Win", false);
+        tmpObjective.text = $"Objective: {cur} / {max} Orders completed";
+
+        if (isWin)
+        {
+            tmpObjective.text = $"Objective Complete!!";
+        }
+    }
+}
